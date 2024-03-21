bee3e988fcc740bd5dbcf931ae6ee81a6ed076bd
Vitor Marinho <vitormarinhoh@gmail.com>
2024-03-21 16:20:05 -0300

    Adicionado componentes de telas

--- src/App.tsx
+++
@@ -1,8 +0,0 @@
-import telaLogin from "./pages/Registro/telaRegistro"
-import { BrowserRouter } from 'react-router-dom'
-
-function App() {
-    return{
-        <BrowserRouter></BrowserRouter>
-    }
-}
